6e1ab1b6f2168663c141f0b1bdeed4c418a5f5df
Paulo Filho <pcf.feat@gmail.com>
2023-02-13 09:00:01 -0400

    study: create visible entry point powered by flutter material

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Flutterando',
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
