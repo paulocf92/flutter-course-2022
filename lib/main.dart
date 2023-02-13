7bb6729aceaf727435371401a8a59749ca487633
Paulo Filho <pcf.feat@gmail.com>
2023-02-13 10:50:21 -0400

    study: convert widgets into separate components

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Flutterando'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('Clicked');
-        },
-      ),
-    );
-  }
 }
