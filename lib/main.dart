6697df502baa96c839a6820a75e2d223f952c57e
Paulo Filho <pcf.feat@gmail.com>
2023-02-15 15:07:07 -0400

    study: introducing Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
 
-import 'myapp.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(const MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Curso Flutter'),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.red,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.green,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
 }
