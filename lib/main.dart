16cc8daabf1643d6d670cc6221ca5cc17e8a293a
Paulo Filho <pcf.feat@gmail.com>
2023-02-15 16:27:45 -0400

    chore: blank app

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final controller = TextEditingController();
-  final list = <String>[];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,40 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text('Curso Flutter'),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: controller,
-                    ),
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        final text = controller.text;
-                        setState(() {
-                          list.add(text);
-                        });
-                        controller.clear();
-                      },
-                      icon: const Icon(Icons.add)),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  final item = list[index];
-                  return ListTile(title: Text(item));
-                },
-              ),
-            ),
-          ],
-        ),
+        body: Container(),
       ),
     );
   }
